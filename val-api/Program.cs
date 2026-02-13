f4a3f7e92e7fb8f368ddd7707c8976291c703ee8
HansVilladiego <C1182-23@itmlyceumalabang.onmicrosoft.com>
2026-02-14 05:16:03 +0800

    SQLite connection

--- val-api/Program.cs
+++ val-api/Program.cs
@@ -1,21 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using RandomQuoteApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        policy => policy.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+});
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-
-
-app.Run();
-
-
